050a49a69093c25e167b6d9d77f2fe8b5b28681f
tmcdaniel787 <54695239+tmcdaniel787@users.noreply.github.com>
2019-10-10 21:54:34 -0500

    Add files via upload

---
+++ Dice/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Exp(Math.Log(10) * 4));
+            Console.WriteLine(Max(4, 2, 5, 1));
+
+        }
+        public static int Max(int a, int b)
+        {
+            return Math.Max(a, b);
+        }
+        public static int Max(int a, int b, int c)
+        {
+            return Math.Max(Max(a, b),c);
+        }
+            public static int Max(int a, int b, int c, int d)
+        {
+            return Max(Max(a, b, c),d);
+        }
+    }
+}
---
+++ Dice/ConsoleApp2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numRolls;
+            Console.WriteLine("Enter number of rolls:");
+            if (int.TryParse(Console.ReadLine(), out numRolls))
+            {
+
+            
+            int dice1, dice2, dice3, dice4, dice5, dice6;
+            DiceRoll(numRolls, out dice1, out dice2, out dice3, out dice4, out dice5, out dice6);
+            Console.WriteLine($"Dice 1: {dice1:N0}");
+            Console.WriteLine($"Dice 2: {dice2:N0}");
+            Console.WriteLine($"Dice 3: {dice3:N0}");
+            Console.WriteLine($"Dice 4: {dice4:N0}");
+            Console.WriteLine($"Dice 5: {dice5:N0}");
+            Console.WriteLine($"Dice 6: {dice6:N0}");
+            }
+        }
+        static void DiceRoll(int numberOfRolls, out int dice1, out int dice2, out int dice3, out int dice4, out int dice5, out int dice6)
+        { 
+            Random random = new Random();
+            dice1 = 0;
+            dice2 = 0;
+            dice3 = 0;
+            dice4 = 0;
+            dice5 = 0;
+            dice6 = 0;
+            
+            for (int i=1; i<=numberOfRolls;i++)
+            {
+                int dice = random.Next(1, 7);
+                switch(dice)
+                {
+                    case 1:
+                        dice1++;
+                        break;
+                    case 2:
+                        dice2++;
+                        break;
+                    case 3:
+                        dice3++;
+                        break;
+                    case 4:
+                        dice4++;
+                        break;
+                    case 5:
+                        dice5++;
+                        break;
+                    case 6:
+                        dice6++;
+                        break;
+                }
+
+            }
+            
+        }
+        
+    }
+}
